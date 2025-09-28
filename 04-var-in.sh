f9ef1b567a0f2065fa9d96a6a354c4e809e4ccd2
sravanthi <sravanthi.kps@gmail.com>
2025-09-28 23:11:05 +0530

    varinsid

---
+++ 04-var-in.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2 how r u"
+echo "$PERSON2:: Hi $PERSON1 im good"
+echo "$PERSON1:: $PERSON2 r u in hyd"
+echo "$PERSON2:: no, $PERSON1"
